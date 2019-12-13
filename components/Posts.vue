1d1bc9ba36241e776f4e702e1d6a2f29f92f4d82
FIAP <FIAP>
2019-12-12 22:48:46 -0300

    finalizando de app com nuxt, utilizando axios para api de posts

---
+++ components/Posts.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="main-content">
+    <div class="container">
+      <h1 class="title has-text-centered">Recent Posts.</h1>
+      <div class="columns is-multiline">
+        <div class="column is-half" v-for="post in posts" :key="post.id">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">{{post.title}}</p>
+            </header>
+            <div class="card-content">
+              <div class="content">
+                {{post.body}}
+                <br />
+                <small>
+                  by
+                  <strong>usuário: {{post.userId}}</strong>
+                  \\ id do post {{post.id}}
+                </small>
+              </div>
+            </div>
+            <footer class="card-footer">
+              <nuxt-link :to="`/post/${post.id}`" class="card-footer-item">Read More</nuxt-link>
+            </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "posts",
+    created() {
+        return axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then(resp => {
+                this.posts = resp.data
+            })
+            .catch(error => {
+                error({ statusCode: 404, message: "Post not found" });
+                console.log(`Erro: ${error}`);
+            })
+    },
+    data() {
+        return {
+            posts: []
+        }
+    }
+}
+</script>
